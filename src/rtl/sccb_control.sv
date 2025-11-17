acf2614349b021e7ee277b01f21c2f2c3b9df4bc
masonmiao-cs124 <mqmiao2@illinois.edu>
2025-11-17 10:31:36 -0600

    added some states

--- src/rtl/sccb_control.sv
+++ src/rtl/sccb_control.sv
@@ -36,13 +36,39 @@ module sccb_control(
     enum logic [10:0] { 
         s_start,
         s_write_7,
+        s_write_7_2,
+        s_write_7_3,
+
+
         s_write_6,
+        s_write_6_2,
+        s_write_6_3,
+      
+      
         s_write_5,
+        s_write_5_2,
+        s_write_5_3,
+
         s_write_4,
+        s_write_4_2,
+        s_write_4_3,
+
         s_write_3,
+        s_write_3_2,
+        s_write_3_3,
+
         s_write_2,
+        s_write_2_2,
+        s_write_2_3,
+
         s_write_1,
+        s_write_1_2,
+        s_write_1_3,
+
         s_write_0,
+        s_write_0_2,
+        s_write_0_3,
+
         ack,
         s_idle
     } curr_state, next_state;
@@ -51,15 +77,21 @@ module sccb_control(
     logic [7:0] reg_data;
     logic [15:0] dout;
     logic [7:0] counter;
-   
+
+    logic addr_or_data; //not doing anything with this rn
+    
+
     assign reg_addr = dout[15:8];
     assign reg_data = dout[7:0];
 
     always_comb
     begin
+        if (reset) begin
+        addr_or_data = 0; // sets it to read only the addr
+        end
         next_state = s_idle;
         
-        scl = 0; //when this is 0, the voltage is HIZ, pulled up!
+        scl = 1; 
         //sda = 0;, probably cant assign?
         reg_addr = 8'h0;
         reset = 0;
@@ -69,14 +101,167 @@ module sccb_control(
             begin
                 sda = 0;
             end
-            add_1:
+            s_write_7:
             begin
-             
+                scl = 0;
+                if (~addr_or_data)begin
+                sda = reg_addr[7];
+                end
+                else begin
+                sda = reg_data[7];
+                end
             end
-            add_2:
+            s_write_7_2:
             begin
-                
+                scl = 1;
             end
+            s_write_7_3:
+            begin
+                scl = 0;
+            end
+
+            s_write_6:
+            begin
+                scl = 0;
+                if (~addr_or_data)begin
+                sda = reg_addr[6];
+                end
+                else begin
+                sda = reg_data[6];
+                end
+            end
+            s_write_6_2:
+            begin
+                scl = 1;
+            end
+            s_write_6_3:
+            begin
+                scl = 0;
+            end
+
+            s_write_5:
+            begin
+                scl = 0;
+                if (~addr_or_data)begin
+                sda = reg_addr[5];
+                end
+                else begin
+                sda = reg_data[5];
+                end
+            end
+            s_write_5_2:
+            begin
+                scl = 1;
+            end
+            s_write_5_3:
+            begin
+                scl = 0;
+            end
+
+
+            s_write_4:
+            begin
+                scl = 0;
+                if (~addr_or_data)begin
+                sda = reg_addr[4];
+                end
+                else begin
+                sda = reg_data[4];
+                end
+            end
+            s_write_4_2:
+            begin
+                scl = 1;
+            end
+            s_write_4_3:
+            begin
+                scl = 0;
+            end
+
+
+            s_write_3:
+            begin
+                scl = 0;
+                if (~addr_or_data)begin
+                sda = reg_addr[3];
+                end
+                else begin
+                sda = reg_data[3];
+                end
+            end
+            s_write_3_2:
+            begin
+                scl = 1;
+            end
+            s_write_3_3:
+            begin
+                scl = 0;
+            end
+
+
+            s_write_2:
+            begin
+                scl = 0;
+                if (~addr_or_data)begin
+                sda = reg_addr[2];
+                end
+                else begin
+                sda = reg_data[2];
+                end
+            end
+            s_write_2_2:
+            begin
+                scl = 1;
+            end
+            s_write_2_3:
+            begin
+                scl = 0;
+            end
+
+
+            s_write_1:
+            begin
+                scl = 0;
+                if (~addr_or_data)begin
+                sda = reg_addr[1];
+                end
+                else begin
+                sda = reg_data[1];
+                end
+            end
+            s_write_1_2:
+            begin
+                scl = 1;
+            end
+            s_write_1_3:
+            begin
+                scl = 0;
+            end
+
+            s_write_0:
+            begin
+                scl = 0;
+                if (~addr_or_data)begin
+                sda = reg_addr[0];
+                end
+                else begin
+                sda = reg_data[0];
+                end
+            end
+            s_write_0_2:
+            begin
+                scl = 1;
+            end
+            s_write_0_3:
+            begin
+                scl = 0;
+            end
+
+
+
+
+
+            
         endcase
 
         case (curr_state)
@@ -88,8 +273,84 @@ module sccb_control(
             s_start: if (!sda) begin
                 next_state = s_write_7;
             end
-            s_ack: if (!sda) begin
+            s_write_7: begin
+                next_state = s_write_7_2; 
+            end
+            s_write_7_2: begin
+                next_state = s_write_7_3;
+            end
+            s_write_7_3: begin
+                next_state = s_write_6;
+            end
+            s_write_6: begin
+                next_state = s_write_6_2; 
+            end
+            s_write_6_2: begin
+                next_state = s_write_6_3;
+            end
+            s_write_6_3: begin
+                next_state = s_write_5;
+            end
+            s_write_5: begin
+                next_state = s_write_5_2; 
+            end
+            s_write_5_2: begin
+                next_state = s_write_5_3;
+            end
+            s_write_5_3: begin
+                next_state = s_write_4;
+            end
+            s_write_4: begin
+                next_state = s_write_4_2; 
+            end
+            s_write_4_2: begin
+                next_state = s_write_4_3;
+            end
+            s_write_4_3: begin
+                next_state = s_write_3;
+            end
+            s_write_3: begin
+                next_state = s_write_3_2; 
+            end
+            s_write_3_2: begin
+                next_state = s_write_3_3;
+            end
+            s_write_3_3: begin
+                next_state = s_write_2;
+            end
+            s_write_2: begin
+                next_state = s_write_2_2; 
+            end
+            s_write_2_2: begin
+                next_state = s_write_2_3;
+            end
+            s_write_2_3: begin
+                next_state = s_write_1;
+            end
+            s_write_1: begin
+                next_state = s_write_1_2; 
+            end
+            s_write_1_2: begin
+                next_state = s_write_1_3;
+            end
+            s_write_1_3: begin
                 next_state = s_write_0;
+            end
+            s_write_0: begin
+                next_state = s_write_0_2; 
+            end
+            s_write_0_2: begin
+                next_state = s_write_0_3;
+            end
+            s_write_0_3: begin
+                next_state = s_ack;
+            end
+
+
+            s_ack: if (!sda) begin
+                addr_or_data = ~addr_or_data;
+                next_state = s_write_0;
+
             end else begin
                 next_state = s_idle;
             end
@@ -105,7 +366,7 @@ module sccb_control(
      OV7670_config_rom camera_rom(
         .clk(clk),
         .addr(addr),
-        .dout()
+        .dout(dout)
     )
 
 endmodule
